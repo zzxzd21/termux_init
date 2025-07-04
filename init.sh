95cddec97e95d60947db77a3c06f84fd7b004952
zzxzd21 <94742273+zzxzd21@users.noreply.github.com>
2025-07-04 19:30:45 +0800

    001930

--- init.sh
+++ init.sh
@@ -3,7 +3,8 @@
 #记得先运行chmod哦~
 
 #安装软件
-pkg update && pkg upgrade -y 
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirror.nju.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list
+apt update && pkg upgrade -y
 pkg install openssh openjdk-17 vim clang python3 htop mandoc -y
 
 #初始化密码便于ssh访问（不安全的做法！！）
@@ -19,7 +20,7 @@ rm ~/spark-3.5.6-bin-hadoop3.tgz
 
 #配置自启动
 echo 'sshd' >> ~/.bashrc
-echo 'ifconfig | grep 192.' >> ~/.bashrc
+echo 'ifconfig | grep "192.168"' >> ~/.bashrc
 echo 'whoami' >> ~/.bashrc
 #结束
 clear
